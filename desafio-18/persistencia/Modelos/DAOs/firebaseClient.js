8628afe39ed4af74ba709dd8c8db54ca52fe3bb4
Antonella Rodríguez <82007150+anrrid@users.noreply.github.com>
2022-09-02 11:08:15 -0300

    Desafio 18

---
+++ desafio-18/persistencia/Modelos/DAOs/firebaseClient.js
@@ -0,0 +1,35 @@
+import DBClient from "./db.js";
+import admin from 'firebase-admin'
+import { firebaseData } from '../../../config.js'
+
+class FirebaseClient extends DBClient {
+    constructor() {
+        super()
+    }
+
+    static admin = admin.initializeApp({
+        credential: admin.credential.cert(firebaseData),
+        databaseURL: "https://ecommerce-10e65-default-rtdb.firebaseio.com"
+    });
+
+    async connect() {
+        try {
+            this.db = admin.firestore()
+            this.connected = true
+        }
+        catch(err) {
+            throw new Error(`Error al conectar con Firebase: ${err}`)
+        }
+    }
+    async disconnect() {
+        try {
+            this.db.terminate()
+            this.connected = false
+        }
+        catch(err) {
+            throw new Error(`Error al desconectar con Firebase: ${err}`)
+        }
+    }
+}
+
+export default FirebaseClient
